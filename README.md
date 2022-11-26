1f5fb46075e81e87039e74b5f75e6895adaf3ba4
benpariswork <88748300+benpariswork@users.noreply.github.com>
2022-11-26 02:51:18 -0800

    Initial push for password gen v1

---
+++ README.md
@@ -0,0 +1,48 @@
+# PWG - Password Generator V1
+
+A simple python script providing cryptographically secure alphanumeric strings. Users are asked how long of a string they would like the script to provide.
+
+
+## Features
+
+-   Uses python's module for cryptographically secure strings: 'secrets'.
+-   Avoids use of 'random' module.
+-   Custom string length.
+
+## Installation
+
+1. ##### Make sure you have Python 3 installed. If you don't have it:
+
+-   [Download it from python.org](https://www.python.org/downloads/)
+
+
+2. ##### Clone the repo:
+
+```
+git clone https://github.com/benpariswork/pwg-v1.git
+```
+
+3. ##### Open the project folder:
+
+```
+cd pwg-v1
+```
+
+4. ##### Check python 3 is installed:
+
+```
+python --version
+```
+
+5. ##### Start a local dev server at `http://localhost:8080`:
+
+```
+python pwg-v1
+```
+
+## Notes
+
+-   Always ensure python is up to date
+-   Feel free to use this as your own password generator
+-   Contributions are welcome
+
---
+++ pwg-v1.py
@@ -0,0 +1,50 @@
+
+
+### PWG - Password Generator V1
+## By: Ben Paris
+
+
+## Import Libraries
+
+# String helps python piece together our password.
+import string
+
+# Secrets provides our password with cryptographically secure numbers.
+import secrets
+
+
+
+## Script
+
+# Ask user for desired password length.
+print("PWG - Welcome to the Password Generator.")
+print("PWG - Please provide an integer from 1-99.") 
+length= input("PWG - How long would you like your password to be?  ") 
+
+# Test the user's input to see if it is an integer.
+if length.isdigit():
+    # Define length as an integer.
+    length = int(length)
+
+    # Test length to see if it is within the defined range (1-99)
+    if length in range(1, 99):
+
+        # Define 'alphabet' with letters and numbers making it alphanumeric. 
+        alphabet = string.ascii_letters + string.digits
+
+        # Create secure alphanumeric password.
+        password = ''.join(secrets.choice(alphabet) for i in range(length))
+
+        # Display the password to the user and ask them to clear the screen.
+        print("PWG - Thank you for using the Password Generator. Please feel free to use it however you see fit.")
+        print("PWG - After recording your new password, please use the command 'clear' to erase your password.")
+        print("PWG - PASSWORD: " + password)
+
+    else:
+
+        # Printer Error message if length is NOT an integer.
+        print("PWG - ERROR - Invalid Input. Please provide an integer from 1-99.")
+
+else: 
+    print("PWG - ERROR - Invalid Input. Please enter an integer.")
+    
